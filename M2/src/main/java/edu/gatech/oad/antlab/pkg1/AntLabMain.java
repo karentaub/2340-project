c358b764e0966315eb890f08f5acf35384483e71
Devany Sandoval <sandovad96@gmail.com>
2017-01-24 22:02:59 -0500

    added Devany

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -1,4 +1,4 @@
-package edu.gatech.oad.antlab.pkg1;
+spackage edu.gatech.oad.antlab.pkg1;
 
 
 import edu.cs2335.antlab.pkg3.*;
@@ -73,8 +73,8 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p4.toString("gburdell4");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
+                  Person5 p5 = new Person5("Devany Sandoval");
+                  toPrint += p5.toString("dsandoval7");
 		  
         System.out.println(toPrint);
         
