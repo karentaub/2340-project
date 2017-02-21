172368c5422e32319c073801bab2429b520ee9d2
Alen Polakof <alenpolakof@lawn-143-215-126-11.lawn.gatech.edu>
2017-02-20 20:02:23 -0500

    Registration XML

---
+++ WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/controllers/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.alenpolakof.waterapp.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.alenpolakof.waterapp.R;
+/**
+ * Created by alenpolakof on 2/12/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
---
+++ WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/controllers/OpeningScreenActivity.java
@@ -0,0 +1,15 @@
+package com.example.alenpolakof.waterapp.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.alenpolakof.waterapp.R;
+
+public class OpeningScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opening_screen);
+    }
+}
