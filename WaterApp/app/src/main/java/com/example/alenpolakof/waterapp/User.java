4f4f46d144f549cadb2e693ae84e3c757f4950fa
alazzaretti3 <alazzaretti@gatech.edu>
2017-02-24 16:43:57 -0500

    added user class tostring

--- WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/User.java
+++ WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/User.java
@@ -67,4 +67,8 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+    @Override
+    public String toString() {
+        return this.username;
+    }
 }
