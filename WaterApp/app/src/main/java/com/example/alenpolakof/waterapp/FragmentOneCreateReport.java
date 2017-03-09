f958d2d609df7eef8df781bc76c9971d0485d302
Alen Polakof <alenpolakof@lawn-143-215-117-254.lawn.gatech.edu>
2017-03-09 00:23:53 -0500

    Google maps done

--- WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/FragmentOneCreateReport.java
+++ WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/FragmentOneCreateReport.java
@@ -4,102 +4,55 @@ package com.example.alenpolakof.waterapp;
  * Created by apolakof on 2/28/17.
  */
 
-import android.*;
-import android.Manifest;
+
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
 import android.widget.Toast;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.server.converter.StringToIntConverter;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.Places;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapFragment;
-import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.util.Date;
-import java.util.Map;
+import java.util.ArrayList;
+
 
 public class FragmentOneCreateReport extends Fragment implements OnMapReadyCallback{
     private GoogleMap mMap;
-    private MapView mapView;
     private SupportMapFragment mSupportMapFragment;
-    private boolean mPermission;
-    private Location mLastLocation;
-    private GoogleApiClient mGoogleApiClient;
     double latitudeToSave, longitudeToSave;
-    double DEFAULT_LONGIDUDE = -84.3963;
-    double DEFAULT_LATTITUDE = 33.7756;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        Log.d("DEVANY", "ONcreate");
-        mGoogleApiClient = new GoogleApiClient.Builder(this.getContext())
-                .addApi(LocationServices.API)
-                .addApi(Places.GEO_DATA_API)
-                .addApi(Places.PLACE_DETECTION_API)
-                .enableAutoManage(this.getActivity(), new GoogleApiClient.OnConnectionFailedListener() {
-                    @Override
-                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-                        // your code here
-                    }
-                })
-                .build();
-        mGoogleApiClient.connect();
         View rootView = inflater.inflate(R.layout.fragment_map, container,
                 false);
         mSupportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         mSupportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                Log.d("DEVANY", "ONMAPREADY111");
                 mMap = googleMap;
-                LatLng gATech = new LatLng(DEFAULT_LATTITUDE,DEFAULT_LONGIDUDE);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(gATech));
                 mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                     @Override
                     public void onMapClick(LatLng latLng) {
                         latitudeToSave = latLng.latitude;
                         longitudeToSave = latLng.longitude;
                         LatLng markerLatLng = new LatLng(latitudeToSave, longitudeToSave);
+                        mMap.clear();
                         mMap.addMarker(new MarkerOptions().position(markerLatLng));
                         mMap.moveCamera(CameraUpdateFactory.newLatLng(markerLatLng));
-                        Toast.makeText(getContext(), "Clicked " + latitudeToSave + ", " + longitudeToSave, Toast.LENGTH_SHORT).show();
-                        Log.d("DEVANY", "Clicked " + latitudeToSave + ", " + longitudeToSave);
-
                     }
                 });
 
-//                updateLocationUI();
-//                getDeviceLocation();
             }
         });
         Button cancel = (Button) rootView.findViewById(R.id.cancel_fragment_one);
@@ -118,79 +71,24 @@ public class FragmentOneCreateReport extends Fragment implements OnMapReadyCallb
                 startActivity(intent);
             }
         });
-
         return rootView;
-
     }
-    @Override
-    public void onStart() {
-        super.onStart();
-        mGoogleApiClient.connect();
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }
+    /*
+     * returns: the longitude
+     */
     public double getLongitudeToSave() {
         return longitudeToSave;
     }
 
+    /*
+     * returns: the latitude
+     */
     public double getLatitudeToSave() {
         return latitudeToSave;
     }
 
-
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        Log.d("DEVANY", "ONMAPREADY");
-        mMap = googleMap;
-        LatLng gATech = new LatLng(DEFAULT_LATTITUDE,DEFAULT_LONGIDUDE);
-        mMap.addMarker(new MarkerOptions().position(gATech).title("GATECH"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(gATech));
-
-       // updateLocationUI();
-       // getDeviceLocation();
+    public void onMapReady(GoogleMap googleMap) {}
 
 
-    }
-
-    private void updateLocationUI() {
-        if(mMap == null){
-            Log.d("Devany", "map is null");
-            return;
-        }
-        if(ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mPermission = true;
-        } else {
-            Log.d("Devany", "Permission not granted");
-            ActivityCompat.requestPermissions(this.getActivity(), new String[] {
-                    Manifest.permission.ACCESS_FINE_LOCATION}, 940);
-
-        }
-    }
-    private void getDeviceLocation() {
-        if(ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mPermission = true;
-        } else {
-            mPermission = false;
-        }
-        if(mPermission) {
-            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        }
-        System.out.println("DEVANY" + latitudeToSave);
-        if(mLastLocation != null){
-            latitudeToSave = mLastLocation.getLatitude();
-            longitudeToSave = mLastLocation.getLongitude();
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                    new LatLng(latitudeToSave, longitudeToSave),5));
-        } else {
-            Log.d("DEVANY", "LOCATION NULL");
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(DEFAULT_LATTITUDE,DEFAULT_LONGIDUDE),5));
-        }
-
-    }
 }
--- WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/model/ReportsMapActivity.java
+++ WaterApp/app/src/main/java/com/example/alenpolakof/waterapp/model/ReportsMapActivity.java
@@ -2,21 +2,14 @@ package com.example.alenpolakof.waterapp.model;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.alenpolakof.waterapp.HomeActivity;
 import com.example.alenpolakof.waterapp.R;
 import com.example.alenpolakof.waterapp.Report;
-import com.example.alenpolakof.waterapp.ReportViewActivty;
 import com.example.alenpolakof.waterapp.TempDB;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
